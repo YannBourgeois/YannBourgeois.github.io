8d98a4ef382ff120f6ad79fc0d0262de512231c3
YannBourgeois <yann.x.c.bourgeois@gmail.com>
2025-02-06 10:13:59 +0000

    Deploying to gh-pages from @ YannBourgeois/YannBourgeois.github.io@77d19d02f3d82324d61047bb8043b012b99e1b51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Current and past research",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
