7324e0380b462a227f4dd2b50c8d31415994a6c9
YannBourgeois <yann.x.c.bourgeois@gmail.com>
2025-02-06 09:06:26 +0000

    Deploying to gh-pages from @ YannBourgeois/YannBourgeois.github.io@7fb7fad32d711b33b680961ffdd3b49e31ef2ec2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,14 +442,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-population-dynamics-of-transposable-elements",
+          title: 'Population dynamics of Transposable Elements',
+          description: "Ongoing project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-population-genomics-of-the-argan-tree",
+          title: 'Population Genomics of the Argan tree',
+          description: "Ongoing project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
